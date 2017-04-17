eb89ffc936c601d3c13b628189f7f6abccdc828d
JJZi <huijieyep@shaw.ca>
2017-04-17 01:24:42 -0600

    add pdf

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,42 +1,48 @@
 import React, { Component } from 'react'
-import { storage } from '../database/database'
-import PdfViewer from './PdfViewer';
+import { storage } from '../database/database';
 
 const storageRef = storage.ref()
 const drawingFile = storageRef.child('Drawings/DCI-100-A.pdf')
 
 class DrawingsPage extends Component {
-constructor() {
+  constructor() {
     super()
-    this.state=({
+    this.state = ({
       url: ""
     })
   }
 
-componentWillMount() {
-  drawingFile.getDownloadURL().then((url) => {
-    if (url !== '') {
-      this.setState({
-        url: url
-      })
-    }
-  }
-  ).catch((error)=> (
-    console.log(error)
-  ))
-}
+  componentWillMount() {
+    drawingFile.getDownloadURL().then((url) => {
+      if (url !== '') {
+        this.setState({
+          url: url
+        });
 
-componentWillUnmount () {
-  storage.ref('Drawings').off()
-}
+      }
+    }
+    ).catch((error) => (
+      console.log(error)
+    ))
+  }
+
+  componentWillUnmount() {
+    storage.ref('Drawings').off()
+  }
 
   render() {
-      return (
-      <div id="drawing">
-       <PdfViewer file={this.state.url} />
+   
+    return (
+      <div id="chart">
+        <div className="chart-wrapper">
+          <iframe src={this.state.url} allowFullScreen>
+            <p><em><strong>ERROR: </strong>
+              An &#105;frame should be displayed here but your browser version does not support &#105;frames.</em> Please update your browser to its most recent version and try again.</p>
+          </iframe>
+        </div>
       </div>
     )
   }
 }
 
-export default DrawingsPage
+export default DrawingsPage;
